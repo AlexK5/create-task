698a55b456091f1e9ee4b92d0735dfad68f0463d
Alex Keri <akeri@ucvts.tec.nj.us>
2019-03-01 10:51:41 -0500

    added line parsing

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 function execute(){
-  console.log(code)
+  let p=document.getElementById("code").innerHTML;
+  let lines=[];
+  let j=0;
+  for(let i = 0; i<p.length; i++){
+    if(p[i]=="\n"){
+      lines.push(p.substring(j,i));
+      j=i+1;
+    }
+  }
+  lines.push(p.substring(j,p.length))
+  console.log(lines);
 }
